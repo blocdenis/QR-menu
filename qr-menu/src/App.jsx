f977b071d017ddc0ebb7c35f8ce3d62716e7091c
Denys Horkavyi <denisdenis2295@gmail.com>
2023-11-12 15:46:25 +0100

    chore:added router

--- qr-menu/src/App.jsx
+++ qr-menu/src/App.jsx
@@ -1,16 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { RouterProvider, Routes, createBrowserRouter } from 'react-router-dom'
 import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <h1>QR-Menu</h1>
-    </>
-  )
+const Root = () => {
+  return <Routes></Routes>
+}
+const router = createBrowserRouter([{ path: '*', Component: Root }])
+const App = () => {
+  return
+  ;<RouterProvider router={router} />
 }
 
 export default App
