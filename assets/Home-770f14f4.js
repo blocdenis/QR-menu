644b8ca565cb043f1f89e1d46c8483ccaada20d2
Denys Horkavyi <denisdenis2295@gmail.com>
2023-11-26 11:57:43 +0100

    Updates

--- assets/Home-770f14f4.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./index-a7dae961.js";const o=()=>r.jsx("div",{style:{backgroundColor:"lightgray",padding:"30px"},children:r.jsx("h2",{children:"Home Page"})});export{o as default};
