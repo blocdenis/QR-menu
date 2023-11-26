4793f5090d2661e4955c5d0ab8adc1fa804c0366
Denys Horkavyi <denisdenis2295@gmail.com>
2023-11-26 11:18:32 +0100

    Updates

---
+++ assets/Menu-0ca81f7c.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-38435cdf.js";const n=()=>r.jsx(r.Fragment,{children:r.jsx("div",{style:{backgroundColor:"lightgray",padding:"30px"},children:r.jsx("h2",{children:"Menu"})})});export{n as default};
