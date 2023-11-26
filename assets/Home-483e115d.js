5848529a488e2d005e39bdbbec4493a1c88bd656
Denys Horkavyi <denisdenis2295@gmail.com>
2023-11-26 11:30:44 +0100

    Updates

---
+++ assets/Home-483e115d.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-f667fa74.js";const o=()=>r.jsx("div",{style:{backgroundColor:"lightgray",padding:"30px"},children:r.jsx("h2",{children:"Home Page"})});export{o as default};
---
+++ assets/Menu-eaa2ce2b.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-f667fa74.js";const n=()=>r.jsx(r.Fragment,{children:r.jsx("div",{style:{backgroundColor:"lightgray",padding:"30px"},children:r.jsx("h2",{children:"Menu"})})});export{n as default};
