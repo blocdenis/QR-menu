6b0715a21831165e0c1c7d2637d0b1a07dbfc317
Denys Horkavyi <denisdenis2295@gmail.com>
2023-12-03 18:12:11 +0100

    Updates

--- assets/Menu-aa9cbff9.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./index-55fa5259.js";const n=()=>r.jsx(r.Fragment,{children:r.jsx("div",{style:{backgroundColor:"lightgray",padding:"30px"},children:r.jsx("h2",{children:"Menu"})})});export{n as default};
---
+++ assets/ModMenu-adcc303f.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-9264bc51.js";const d=()=>r.jsx("div",{style:{backgroundColor:"lightgray",padding:"30px"},children:r.jsx("h2",{children:"mod menu"})});export{d as default};
