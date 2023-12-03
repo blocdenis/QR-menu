dc9d8b8fe6fcaec39389e26ddb9aafd812a05a20
Denys Horkavyi <denisdenis2295@gmail.com>
2023-12-03 17:45:22 +0100

    Updates

---
+++ assets/Dashboard-19ccee37.js
@@ -0,0 +1,1 @@
+import{j as e,r as s,O as t}from"./index-158916c3.js";const i=()=>e.jsxs("div",{style:{display:"flex",flexDirection:"column",minHeight:"100vh",height:"100%"},children:[e.jsx("header",{}),e.jsx("main",{style:{flex:1},children:e.jsx(s.Suspense,{children:e.jsx(t,{})})}),e.jsx("footer",{children:"contact place"})]});export{i as default};
