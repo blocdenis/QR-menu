83d49c2598ed6e9b24a01fc0f94a164502ff2365
LesiaHumenna <lgumenna61@gmail.com>
2023-12-16 01:06:31 +0100

    creat home page(admin)

---
+++ qr-menu/src/pages/Table.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Table() {
+  return (
+    <div style={{ backgroundColor: 'lightgray', padding: '30px' }}>
+      <h2>Table</h2>
+    </div>
+  )
+}
+
+export default Table
