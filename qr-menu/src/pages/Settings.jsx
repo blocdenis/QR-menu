0d37664ea6a4b5e28916bedad474cea5e4a2e31e
LesiaHumenna <lgumenna61@gmail.com>
2023-12-27 21:25:10 +0100

    conect component to Page
    conect component to Page tables/ menu/settings

--- qr-menu/src/pages/Settings.jsx
+++ qr-menu/src/pages/Settings.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import NavbarSystem from '../components/NavbarSystem';
+import HeaderSystem from '../components/HeaderSystem';
+import '../css/scss/setting.scss';
 
 function Setting() {
   return (
-    <div style={{ backgroundColor: 'lightgray', padding: '30px' }}>
-      <h2>Settings</h2>
+    <div className='seting-wrap'>
+        <HeaderSystem/>
+        <NavbarSystem/>
     </div>
   )
 }
--- qr-menu/src/pages/Table.jsx
+++ qr-menu/src/pages/Table.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import HeaderSystem from '../components/HeaderSystem';
 import '../css/scss/table.scss';
 import NavbarSystem from '../components/NavbarSystem';
+import HeaderSystem from '../components/HeaderSystem';
 
 function Table() {
   return (
