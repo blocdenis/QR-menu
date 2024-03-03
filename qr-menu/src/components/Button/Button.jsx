8fdd5a94c7d522dfdf22bafe722ec30ba927c88a
Denys Horkavyi <denisdenis2295@gmail.com>
2024-03-03 23:29:57 +0100

    feat/final view of table

--- qr-menu/src/components/Button/Button.jsx
+++ qr-menu/src/components/Button/Button.jsx
@@ -1,27 +1,3 @@
-// import React from 'react';
-// import { Button } from 'primereact/button';
-// import styles from './Button.module.scss';
-
-// function ButtonSystem() {
-//   return (
-//     <div className={styles.block_btn_system}>
-//       <Button className={styles.btn_back} type="back" value="Back">
-//         Back
-//       </Button>
-//       <Button
-//         className={styles.btn_save}
-//         severity="success"
-//         type="save"
-//         value="Save"
-//       >
-//         Save
-//       </Button>
-//     </div>
-//   );
-// }
-
-// export default ButtonSystem;
-
 import React from 'react';
 import './Button.scss';
 import PlusIcon from '../../assets/PlusIcon.jsx';
