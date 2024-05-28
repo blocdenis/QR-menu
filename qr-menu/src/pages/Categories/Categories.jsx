256232b9b1e4bd27f0b411ac38d84c9d7acbce79
LesiaHumenna <lgumenna61@gmail.com>
2024-05-28 21:03:20 +0200

    add modalDelCategory
    add modalDelCategory in Categor-component

--- qr-menu/src/pages/Categories/Categories.jsx
+++ qr-menu/src/pages/Categories/Categories.jsx
@@ -1,38 +1,45 @@
 import styles from './Categories.module.scss';
-import { useState } from 'react';
-import InputCategor from '../../components/InputCategory/InputCategor';
+import { useState, useEffect } from 'react';
+import ModalDelCategor from '../../components/ModalDelCategor/ModalDelCategor.jsx';
 import { AppLayout } from '../../layouts/AppLayout/AppLayout.jsx';
 import Button from '../../components/Button/Button.jsx';
 import TableCategor from '../../components/TableCategor/TableCategor.jsx';
 function Categories() {
-  // const [categor, setCategor] = useState([
-  //   { value: 'meat', label: 'Meat' },
-  //   { value: 'fish', label: 'Fish' },
-  //   { value: 'salad', label: 'Salad' },
-  // ]);
-
+  const [modalOpenDelCategor, setModalOpenDelCategor] = useState(false);
   const [rowsCategor, setRowsCategor] = useState([]);
   const [modalOpenCategor, setModalOpenCategor] = useState(false);
+  const [isAnyModalOpen, setIsAnyModalOpen] = useState(false);
 
   const openCreatCategor = () => {
     setModalOpenCategor(true);
-    };
-    
+  };
+
   const handleNewCategorySubmit = newCategory => {
     setRowsCategor([...rowsCategor, newCategory]);
     console.log('Get NewCategor');
-  }; // const closeCreatCategor = () => {
-  //   setNewCategor(false);
-  //   setShowCategor(true);
-  // };
-  // const handleChange = e => {
-  //   const selectedValue = e.target.value;
-  //   setSelectedCategory(selectedValue);
-  // };
-
-  // const handleAddCategory = newCategory => {
-  //   setCategories([...categories, newCategory]);
-  // };
+  };
+  const openModalDelCateg = id => {
+    setModalOpenDelCategor(prevState => ({
+      ...prevState,
+      [id]: true,
+    }));
+  };
+  const closeModalCategor = id => {
+    setModalOpenDelCategor(prevState => ({
+      ...prevState,
+      [id]: false,
+    }));
+  };
+  const handleDelateCategor = id => {
+    let delRow = rowsCategor.filter(item => item.id !== id);
+    setRowsCategor(delRow);
+    console.log(delRow);
+    closeModalCategor(id);
+  };
+  useEffect(() => {
+    const anyModalOpen = Object.values(modalOpenDelCategor).some(isOpen => isOpen);
+    setIsAnyModalOpen(anyModalOpen);
+  }, [modalOpenDelCategor]);
   return (
     <AppLayout>
       <div className={styles.container_wrap}>
@@ -54,22 +61,36 @@ function Categories() {
               : '';
 
             return (
-              <div key={item.id} className={`${styles.block_categ} ${styles[`div-color-btn-${item.color}`]}`}>
-                <span >
-                  {categorText}
-                </span>
+              <div key={item.id}>
+                <div
+                  onClick={() => openModalDelCateg(item.id)}
+                  className={`${styles.field_category} ${styles[`div-color-btn-${item.color}`]}`}
+                >
+                  <span>{categorText}</span>
+                </div>
+                {modalOpenDelCategor[item.id] && (
+                  <ModalDelCategor
+                    key={item.id}
+                    closeModal={() => closeModalCategor(item.id)}
+                    id={item.id}
+                    handleDelete={() => handleDelateCategor(item.id)}
+                    categor={item.categor}
+                  />
+                )}
               </div>
             );
           })}
         </div>
-        {modalOpenCategor && (
-          <TableCategor
-            rowsCategor={rowsCategor}
-            setRowsCategor={setRowsCategor}
-            setModalOpenCategor={setModalOpenCategor}
-            onNewCategorySubmit={handleNewCategorySubmit}
-          />
-        )}
+        <div className={isAnyModalOpen ? styles.move_down : ''}>
+          {modalOpenCategor && (
+            <TableCategor
+              rowsCategor={rowsCategor}
+              setRowsCategor={setRowsCategor}
+              setModalOpenCategor={setModalOpenCategor}
+              onNewCategorySubmit={handleNewCategorySubmit}
+            />
+          )}
+        </div>
       </div>
     </AppLayout>
   );
