1d07fa88505c0b7d66a832f03f7b086d0176dcb4
Smefinka <smefinka1@gmail.com>
2024-07-14 20:48:23 +0200

    add icons delete and show

---
+++ qr-menu/src/SVG/IconCheck 2.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+function IconCheck() {
+  return (
+    <div>
+      <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="14"
+              height="15"
+              viewBox="0 0 14 15"
+              fill="none"
+            >
+              <path
+                d="M13.7295 3.04967L5.1222 13.3784L0.330078 8.44938L2.00307 6.82287L4.98872 9.89382L11.937 1.55591L13.7295 3.04967Z"
+                fill="white"
+              />
+            </svg>
+    </div>
+  )
+}
+
+export default IconCheck
